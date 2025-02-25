91aa87f604ee8f553e81712328fd90a347edf098
Faisal Chowdhury <chowdhuryfaisal66@gmail.com>
2025-02-25 13:54:17 +0600

    login authentication done

---
+++ js/login.js
@@ -0,0 +1,24 @@
+
+document.getElementById('login-btn').addEventListener('click' , function(p){
+    p.preventDefault();
+    const accountNumber = document.getElementById('account-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    const convertedPin = parseInt(pinNumber);
+
+
+    if(accountNumber.length != 12 || isNaN(accountNumber) ){
+      alert('invalid account number');
+    }else{
+
+
+        if(pinNumber === 1234){
+            console.log(pinNumber)
+        }else {
+           window.location.href = "index.html"
+        }
+        
+
+    }
+    
+    
+})
